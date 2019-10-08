6957583e5baa479f86f061b1a8b508d83e9211d7
zhangyabo <vuezhang@gmail.com>
2019-10-08 17:05:28 +0800

    [新增]:construction: 字体文件处理

--- demo11/src/index.js
+++ demo11/src/index.js
@@ -1,13 +1,3 @@
-import avatar from './avatar.jpg'
-import './index.css'
-import './main.scss'
-import _ from 'lodash'
+import './assets/fonts/iconfont.css'
 
-
-let img = new Image()
-img.src = avatar
-let root = document.getElementById('root')
-root.appendChild(img)
-
-console.log(new Promise(() => {}))
-console.log(_.join(['a', 'b', 'c']))
+let gettime = 1
--- demo11/webpack.config.js
+++ demo11/webpack.config.js
@@ -1,93 +1,62 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // 将css文件单独打包
 
 module.exports = {
     entry: {
         main: './src/index.js'
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        chunkFilename: '[name].chunk.js'
-    },
-    devtool: 'source-map',
-    devServer: {
-        contentBase: './dist',
-        port: 9000,
-        open: true,
-        hot: true,
-        overlay: true,
-    },
-    optimization: {
-      splitChunks: {
-          chunks: 'all'
-      }
+        publicPath: './', // js的引用路径或者CDN地址
+        path: path.resolve(__dirname, 'dist'), // 打包文件输出目录
+        filename: '[name].bundle.js', // 代码打包后的文件名
+        chunkFilename: '[name].js' // 代码拆分后的文件名
     },
     module: {
         rules: [
             {
-                test: /\.(jpg|gif|jpeg|png)$/,
-                use: {
-                    loader: 'url-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'images', // 文件输出目录
-                        limit: 10240, // 小于 10kb 的打包成base64
-                    }
-                }
-            },
-            {
-                test: /\.(css|scss)$/,
+                test: /\.css$/,
                 use: [
-                    // {
-                        //     loader: MiniCssExtractPlugin.loader // 把 style-loader 换成了 MiniCssExatractPlugin.loader
-                    // },
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 2
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: [require('autoprefixer')]
-                        }
-                    },
-                    'sass-loader'
+                    {loader: MiniCssExtractPlugin.loader},
+                    'css-loader'
                 ]
             },
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
+                test: /\.(eot|woff|ttf|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            name: '[name]-[hash:5].min.[ext]',
+                            limit: 5000, // fonts file size <= 5KB, use 'base64'; else, output svg file
+                            publicPath: 'fonts/',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
             }
+            
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            minify: {
-                removeComments: true, // 删除 html 中的注释
-                collapseWhitespace: true, // 删除空白与换行符
-                minifyCSS: true, // 压缩 内联css
-            },
-            filename: 'index.html',
-            template: 'index.html'
-        }),
         new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            // 打包输出HTML
+            title: '自动生成 HTML',
+            minify: {
+                // 压缩 HTML 文件
+                removeComments: true, // 移除 HTML 中的注释
+                collapseWhitespace: true, // 删除空白符与换行符
+                minifyCSS: true // 压缩内联 css
+            },
+            filename: 'index.html', // 生成后的文件名
+            template: 'index.html', // 根据此模版生成 HTML 文件
+        }),
         new MiniCssExtractPlugin({
             filename: '[name].css',
             chunkFilename: '[id].css'
-        }),
-        new OptimizeCssAssetsPlugin({
-            assetNameRegExp: /\.css$/g,
-            cssProcessor: require('cssnano'), // 用于优化/最小化 css 的css处理器， 默认为 cssnanno
-            cssProcessorOptions: {preset: ['default', {discardComments: {removeAll: true}}]}, //传递给 cssProcessor 的选项，默认为{}
-            canPrint: true // 布尔值，指示插件是否可以将消息打印到控制台，默认为 true
         })
     ]
 }
+
