902abacfd31a5a8509760cd8f9004369503881bc
zhangyabo <vuezhang@gmail.com>
2019-10-16 15:59:00 +0800

    [新增]:sparkles: 开发和生产模式·实战

--- demo13/src/index.js
+++ demo13/src/index.js
@@ -1,1 +1,17 @@
-console.log('js')
+
+import './assets/style/base.scss'
+import './assets/fonts/iconfont.css' // 引入字体文件
+
+
+// 发送跨域请求
+$.get(
+    '/comments/hotflow',
+    {
+        id: '4263554020904293',
+        mid: '4263554020904293',
+        max_id_type: '0'
+    },
+    function(data) {
+        console.log(data)
+    }
+)
